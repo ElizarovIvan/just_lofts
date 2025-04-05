2408c3e4addcec5d0f055ea0b7df645fb2786e83
Иван Елизаров <ivanelizarov@Ivans-MacBook-Air.local>
2025-04-05 10:53:23 +0300

    Исправил импорт PrismaAdapter на @next-auth/prisma-adapter

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,7 +2,7 @@ export const dynamic = 'force-dynamic';
 
 import NextAuth from "next-auth";
 import EmailProvider from "next-auth/providers/email";
-import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
