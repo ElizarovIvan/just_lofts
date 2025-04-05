3335389ffd6729c0b93b143b1c582737c45a3f94
Иван Елизаров <ivanelizarov@Ivans-MacBook-Air.local>
2025-04-05 22:39:25 +0300

    Временный перенос NextAuth в nextauth/route.js для отладки

---
+++ app/api/auth/nextauth/route.js
@@ -0,0 +1,27 @@
+export const dynamic = "force-dynamic";
+
+import NextAuth from "next-auth";
+import EmailProvider from "next-auth/providers/email";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "../../../../lib/prisma";
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: Number(process.env.EMAIL_SERVER_PORT),
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  debug: true,
+});
+
+export { handler as GET, handler as POST };
